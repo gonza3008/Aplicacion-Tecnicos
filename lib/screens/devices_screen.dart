8af24f324a9bb250d1859215838f495b1a26be0b
gonza <gonzalo.lopez@sitrack.com>
2022-11-14 09:55:09 -0300

    se elimina DbLite y agrega pantalla device screen

--- lib/screens/devices_screen.dart
+++ lib/screens/devices_screen.dart
@@ -1,95 +1,78 @@
-import 'package:app_tecnicos/models/models.dart';
 import 'package:flutter/material.dart';
 
-import 'package:app_tecnicos/providers/db_provider.dart';
-
-
 class DeviceScreen extends StatelessWidget {
-   
   const DeviceScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-  var products = [
-    Product(1, "Laptop Uno", "Un laptop muy eficiente", 2000),
-    Product(2, "Laptop Dos", "Este lapto está en descuento", 1500),
-    Product(3, "Laptop Tres", "El laptop más caro", 8000)
-  ];
     return Scaffold(
-      body: Center(
-         child: Column(
+        body: Column(
+      children: [
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              const Text(
+                'Seleccione un dispositivo',
+                style: TextStyle(fontSize: 20),
+              )
+            ],
+          ),
+        ),
+
+        //Primera carta
+        Expanded(
+            child: Row(
+          // ignore: prefer_const_literals_to_create_immutables
           children: [
-            ElevatedButton(
-              onPressed: () async {
-                await addTodb(products.last);
-                
-                // ignore: use_build_context_synchronously
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('Producto agregado!'),
-                    duration: Duration(milliseconds: 1000)
-                  ),
-                );
-                 
-                  print( await DBProvider.instance.getAllItems());
-                  const DbData();
-                
-              }, 
-              child: const Text('Path')
-            ),
-            Text('hola'),
-            Column(
-              children: [
-                DbData(),
-              ],
-            ),
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.red,
+                image: ('lib/assets/ican.jpg'),
+              ),
+              margin: const EdgeInsets.only(
+                  left: 10.0, right: 10.0, bottom: 10, top: 20),
+            )),
+            //segunda carta
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.yellow),
+              margin: const EdgeInsets.only(
+                  left: 10.0, right: 10.0, bottom: 10, top: 20),
+            ))
           ],
-         )
-      ),
-    );
-  
-  }
-}
+        )),
 
-Future <void> addTodb (Product product) async{
-  final item = CartItem(id: product.id, name: product.name, price: product.price, quantity: 1);
-  await DBProvider.instance.insert(item);
-}
-
-class DbData extends StatelessWidget {
-  const DbData({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: DBProvider.instance.getAllItems(),
-      builder: ( 
-        BuildContext context, 
-        AsyncSnapshot<dynamic> snapshot) {  
-          if (snapshot.hasData) {
-            List<CartItem> cartItems = snapshot.data!;
-            return cartItems.isEmpty
-            ? const Center(
-              child: Text('No hay data'),
-            ) 
-            : ListView.separated(
-              itemBuilder: (context, index) {
-                return Container(
-                  child: Text(cartItems[index].name)
-                );
-              },
-              separatorBuilder: (context, index) => 
-                const Divider(
-                  height: 10,
-                ),
-              itemCount: cartItems.length,
-            ); 
-
-          } else {
-            return const Center(
-              child: Text('No hay data'),
-            );
-          }
-        });
+        /*NUEVA FILA */
+        Expanded(
+            child: Row(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.blue),
+              margin: const EdgeInsets.only(
+                  left: 10.0, right: 10.0, bottom: 10, top: 20),
+            )),
+            //segunda carta
+            Expanded(
+                child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.green),
+              margin: const EdgeInsets.only(
+                  left: 10.0, right: 10.0, bottom: 10, top: 20),
+            ))
+          ],
+        )),
+      ],
+    ));
   }
 }
